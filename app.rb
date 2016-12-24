55b8abd7276f2f163547a9b52b70088997898e35
kboygit <heybisayangdako@gmail.com>
2016-12-24 12:29:42 -0800

    cleaned app.rb

--- app.rb
+++ app.rb
@@ -1,11 +1,9 @@
-require 'bundler'
-Bundler.require
+# require 'bundler'
+# Bundler.require
 
 class IdeaBoxApp < Sinatra::Base
   get '/' do
     "Hello, World!"
   end
-
-  run! if app_file == $0
-
 end
+  # run! if app_file == $0
