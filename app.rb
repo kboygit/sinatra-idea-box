f1fff599e358743d97dfaaabeb8f0c102649bfea
kboygit <heybisayangdako@gmail.com>
2016-12-24 13:06:13 -0800

    render template

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@
 
 class IdeaBoxApp < Sinatra::Base
   get '/' do
-  "<h1>Hello, World!</h1><blockquote>I guess I always felt even if the world came to an end, McDonald's would still be open.<cite>Susan Bet Pfeffer</cite></blockquote>"
+    erb :index
   end
 end
   # run! if app_file == $0
